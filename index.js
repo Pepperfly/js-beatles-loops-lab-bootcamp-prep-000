acea767897c2c87e7434c8fd66fcd20e64c8480c
Jeff Carter <jeff@pepperfly.com>
2018-09-17 21:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts(factsArr) {
-/*
-for (let i = 0; i = factsArr.length - 1; i++) {
+for (let i = 0; i < factsArr.length - 1; i++) {
     factsArr[i] = factsArr[i] + "!!!";
   }
-*/
   return factsArr;
 }
 
