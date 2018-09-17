ae3b371e8c5e6c48fe1f74b1103e393ab18f69e2
Jeff Carter <jeff@pepperfly.com>
2018-09-17 21:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var playArr = [];
+  
+}
+
+
+function johnLennonFacts(factsArr) {
+/*  for (let i = 0; i = factsArr.length - 1; i++) {
+    factsArr[i] = factsArr[i] + "!!!";
+  } */
+  return factsArr;
+}
+
+
+function iLoveTheBeatles(n) {
+  var loveArr = [];
+  do {
+    loveArr.push('I love the Beatles!');
+    n++;
+  } while (n < 15);
+  return loveArr;
+}
+
